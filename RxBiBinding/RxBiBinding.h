ca6ad48452b0c08e56355f27b2c075bcb53da3f7
hikaruna <me@hikaruna.net>
2018-10-02 01:25:18 +0900

    add cartahge support

---
+++ RxBiBinding/RxBiBinding.h
@@ -0,0 +1,19 @@
+//
+//  RxBiBinding.h
+//  RxBiBinding
+//
+//  Created by 中川輝 on 2018/10/02.
+//  Copyright © 2018年 rxswiftcommunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxBiBinding.
+FOUNDATION_EXPORT double RxBiBindingVersionNumber;
+
+//! Project version string for RxBiBinding.
+FOUNDATION_EXPORT const unsigned char RxBiBindingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxBiBinding/PublicHeader.h>
+
+
